4e5673c0c47f053f65142005ccf828717f687947
user <user@user.com>
2022-08-25 16:21:57 +0300

    update

--- main.py
+++ main.py
@@ -43,13 +43,16 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 num_classes = 4
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Rescaling(1./255, input_shape=(config.img_height, config.img_width,3)),
+  tf.keras.layers.RandomRotation(0.1),
+  tf.keras.layers.RandomZoom(0.1),
+  tf.keras.layers.Rescaling(1./255),
   tf.keras.layers.Conv2D(16, 5, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(num_classes)
