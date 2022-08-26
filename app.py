cd72b9cabc15199ff78b8c634e7bd7a090a63270
user <user@user.com>
2022-08-26 14:13:42 +0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 # from prediction import predict
 import base64
+import sys
 from flask_cors import CORS
 import workWithImage
 app = Flask(__name__)
@@ -12,18 +13,22 @@ def index():
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
+   # print("Vanya lox")
+   # print(request)
    if request.method == 'POST':
       try:
          f = request.files['file']
          f.save("temp/file")
       except:
+
          fi = request.get_json()["file"].split(",")[-1]
          fi = base64.b64decode(fi)
          f = open("temp/file","wb")
          f.write(fi)
          f.close()
-
-      return workWithImage.get_type("temp/file")
+      response = workWithImage.get_type("temp/file")
+      print(response)
+      return response
 
 
 if __name__ == '__main__':
